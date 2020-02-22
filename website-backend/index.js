9d232e69b5cf03df655237df71b80ea94f5816dc
unknown <neubauersim@gmail.com>
2020-02-22 20:33:54 +0100

    update

--- website-backend/index.js
+++ website-backend/index.js
@@ -3,23 +3,6 @@ console.log('Node App startet...')
 const express = require('express');
 const app = express();
 const path = require('path');
-const createError = require('http-errors');
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-app.use(express.urlencoded({ extended: false }));
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'website')));
