3878f65ab36165fb9c492c6cef88ca309a467845
unknown <neubauersim@gmail.com>
2020-02-22 20:12:35 +0100

    update

--- website-backend/index.js
+++ website-backend/index.js
@@ -4,13 +4,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(function(err, req, res, next) {
-  res.redirect('/404');
-  next(err); // <- I don't know enough about ExpressJS to know if this is actually needed. :-D
+// Point static path to dist
+app.use((err, req, res, next) => {
+  express.static(path.join(__dirname, 'website'));
+  next(err);
 });
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'website')));
 
 app.get('*', function (req, res) {
   res.sendFile('website/index.html', { root: __dirname });
