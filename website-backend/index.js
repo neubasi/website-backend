a92184959ec560dc3caf61a1e893e88187ffe9a2
unknown <neubauersim@gmail.com>
2020-02-22 20:30:27 +0100

    update

--- website-backend/index.js
+++ website-backend/index.js
@@ -3,7 +3,13 @@ console.log('Node App startet...')
 const express = require('express');
 const app = express();
 const path = require('path');
+const createError = require('http-errors');
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+app.use(express.urlencoded({ extended: false }));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'website')));
