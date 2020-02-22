65f6ecabedc7409f493a3d918130bf8be20945aa
unknown <neubauersim@gmail.com>
2020-02-22 20:31:51 +0100

    update

--- website-backend/index.js
+++ website-backend/index.js
@@ -10,6 +10,16 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 app.use(express.urlencoded({ extended: false }));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'website')));
