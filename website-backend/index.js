9623269fcf8e6ee81895a28b7abf1f965f442459
unknown <neubauersim@gmail.com>
2020-07-30 14:09:48 +0200

    update

--- website-backend/index.js
+++ website-backend/index.js
@@ -5,14 +5,19 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'website')));
-// app.use('/', express.static(path.join(__dirname, 'website')));
+app.use(express.static(path.join(__dirname, 'stationscockpit')));
+
+app.get('/stationscockpit', function (req, res) {
+  res.sendFile('/stationscockpit/index.html', { root: __dirname });
+});
 
 app.get('*', function (req, res) {
   res.sendFile('/website/index.html', { root: __dirname });
-  });
+});
+
 
 // Server 
 app.listen(80, function () {
-    console.log('Example app listening on port 80!');
+  console.log('Example app listening on port 80!');
 });
 
